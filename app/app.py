c0c10960f766d27bb5f580c1db7a48520854f18b
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2025-02-14 05:57:45 +0700

    fix(ecotrack): gunicorn setup

--- app/app.py
+++ app/app.py
@@ -66,4 +66,5 @@ def submit_form():
     return jsonify({"message": "Form submitted successfully!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
